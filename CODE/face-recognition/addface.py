48c6cc3560b5f43ed2940accfae7ee10af6434d7
小宇 <1917871807@qq.com>
2023-04-08 01:05:01 +0800

    code update

--- CODE/face-recognition/addface.py
+++ CODE/face-recognition/addface.py
@@ -1,17 +1,21 @@
 import cv2
 import face_recognition
 import os
-
 def addface():
     # Initialize OpenCV video capture
-    video_capture = cv2.VideoCapture(0)
+   # video_capture = cv2.VideoCapture(0)
+    video_capture = cv2.VideoCapture("rtsp://admin:a12345678@192.168.136.156:554/stream1")
     # Take picture using video capture
     ret, frame = video_capture.read()
-    # resize frame to 1/4 size for faster face recognition processing
-    frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    target_size = (1080, 1080)
+    # resize frame
+    frame = cv2.resize(frame, target_size)
     # improve face detection by converting to grayscale
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     face_locations = face_recognition.face_locations(frame)
+    # just get the face from the frame with a margin of 10 pixels
+    for (top, right, bottom, left) in face_locations:
+        frame = frame[top-10:bottom+10, left-10:right+10]
     # If no faces are detected, return
     if not face_locations:
         print("No face detected")
--- CODE/face-recognition/recognize.py
+++ CODE/face-recognition/recognize.py
@@ -2,8 +2,39 @@ import cv2
 import os
 import face_recognition
 import numpy as np
+import threading
+# Define the desired size for face detection and recognition
+# target_size = (128, 128)
+# target_size = (300, 300)
+target_size = (1500, 1500)
+frame_stack = []
+width = 0
+height = 0
+
+def get_video():
+    global width, height, frame_stack
+    # Initialize video capture from RTSP stream
+    cap = cv2.VideoCapture("rtsp://admin:a12345678@192.168.136.156:554/stream1")
+    # get the width and height of frame
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    # create a thread to read frames from the video stream and push int to frame_stack
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame_stack.append(frame)
+    cap.release()
+
+def response(state,name,confidence):
+    if state == True:
+        print(name + " " + str(confidence))
+    else:
+        print("no face")
 
 def detect_faces():
+    global frame_stack, width, height
+    # Load face images from faceImg folder
     known_face_encodings = []
     known_face_names = []
     pictures = len(os.listdir('faceImg'))
@@ -13,54 +44,38 @@ def detect_faces():
         face_encoding = face_recognition.face_encodings(img)[0]
         known_face_encodings.append(face_encoding)
         known_face_names.append(f'Person {i}')
-    # Start the video capture
-    cap = cv2.VideoCapture(0)
-
+    # create a thread to read frames from the frame_stack and detect faces
+    t = threading.Thread(target=get_video)
+    t.daemon = True
+    t.start()
     while True:
-        # Read a frame from the video capture
-        ret, frame = cap.read()
-
-        # Convert the frame to RGB format
-        rgb_frame = frame[:, :, ::-1]
-
-        # Detect the faces in the frame
-        face_locations = face_recognition.face_locations(rgb_frame)
-        face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
-
-        # Loop through each detected face
-        for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-            # Compare the face encoding with the known face encodings
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-            name = "Unknown"
-
-            # Find the known face with the highest confidence
-            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)
-
-            if matches[best_match_index]:
-                if face_distances[best_match_index] < 0.4:
-                    name = known_face_names[best_match_index]
-                # Calculate confidence
+        # Resize the frame to the desired size for face detection and recognition
+        if len(frame_stack) == 0:
+            continue
+        frame = frame_stack.pop()
+        frame_stack = []
+        small_frame = cv2.resize(frame, target_size)
+        # Find all the faces in the resized frame
+        face_locations = face_recognition.face_locations(small_frame)
+        face_encodings = face_recognition.face_encodings(small_frame, face_locations)
+        # Loop through each detected face and check if it matches any of the known faces, if so, call response function,no need to draw rectangle
+        # if no face detected, call response function with state = False
+        if len(face_locations) == 0:
+            response(False,"",0)
+        else:
+            for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+                # Compare the face encoding with the known face encodings
+                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+                name = "Unknown"
+                # Find the known face with the highest confidence
+                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                # calculate confidence,if confidence is higher than 0.5, call response function with state = True
                 confidence = 1 - face_distances[best_match_index]
-
-                # Draw a box around the face
-                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-                # Draw the name and confidence above the face
-                cv2.rectangle(frame, (left, top - 35), (right, top), (0, 0, 255), cv2.FILLED)
-                font = cv2.FONT_HERSHEY_DUPLEX
-                cv2.putText(frame, name + ' ' + str(round(confidence * 100, 2)) + '%', (left + 6, top - 6), font, 0.6,
-                            (255, 255, 255), 1)
-
-        # Display the video feed
-        cv2.imshow('Video', frame)
-
-        # Exit the loop when the 'q' key is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # Release the video capture
-    cap.release()
-    cv2.destroyAllWindows()
+                if matches[best_match_index] and confidence > 0.5:
+                    name = known_face_names[best_match_index]
+                    response(True,name,confidence)
+                else:
+                    response(True,"unknown",confidence)
 
 detect_faces()
