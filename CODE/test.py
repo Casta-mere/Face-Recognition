45921c4716d9629afd3779382161a4f9caa4b40a
韦杨婧 <981193371@qq.com>
2023-04-11 20:55:46 +0800

    登录界面

--- CODE/test.py
+++ CODE/test.py
@@ -1,20 +1,23 @@
-from flask import Flask, render_template, request,Response,jsonify,redirect,url_for
+from flask import Flask, render_template, request,Response,jsonify,redirect,url_for,globals
 from flask_cors import CORS
 import json
 import cv2
 import numpy
 import base64
+import control
 app = Flask(__name__) 
+ 
+globals.loginState=0
+globals.password="123456"
+globals.retry=0
  
 @app.route('/',methods=['GET','POST'])
 def hello_world():
     return render_template('firstPage.html')
- 
- 
+
 @app.route('/imgUpload',methods=['GET','POST'])
 def imageUpload():
     return render_template('faceRecog.html')
-
 
 @app.route('/receiveImage', methods=["POST"])
 def receive_image():
@@ -44,6 +47,29 @@ def sendInfo():
     print('name:{}'.format(name),'email:{}'.format(email))
     return redirect(url_for('hello_world'))
  
+@app.route('/manageInfo',methods=['GET','POST'])
+def manageInfo():
+    return render_template('manageInfo.html')
+
+@app.route('/login',methods=['POST','GET'])
+def login():
+    if globals.loginState==0:
+        return render_template(
+            'login.html',
+            retry=globals.retry
+            )
+    else:
+        return redirect(url_for('hello_world'))
+
+@app.route('/loginCheck',methods=['POST','GET'])
+def loginCheck():
+    password=request.form.get('pw')
+    print(password)
+    if globals.password==password:
+        return redirect(url_for('hello_world'))
+    else:
+        globals.retry=1
+        return redirect(url_for('login'))
+
 if __name__ == '__main__':
- 
     app.run(debug=True,host='0.0.0.0',port=5000)
