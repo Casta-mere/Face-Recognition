a251d1b7e8f5b92974dd98e809f5b681f6ea760a
韦杨婧 <981193371@qq.com>
2023-04-12 10:32:58 +0800

    405Over

--- CODE/test.py
+++ CODE/test.py
@@ -11,14 +11,20 @@ globals.loginState=0
 globals.password="123456"
 globals.retry=0
  
+# 首页
 @app.route('/',methods=['GET','POST'])
 def hello_world():
-    return render_template('firstPage.html')
+    return render_template(
+        'firstPage.html',
+        state=globals.loginState
+        )
 
+# 录入人脸界面
 @app.route('/imgUpload',methods=['GET','POST'])
 def imageUpload():
     return render_template('faceRecog.html')
 
+# 接收人脸图片并保存
 @app.route('/receiveImage', methods=["POST"])
 def receive_image():
  
@@ -37,6 +43,7 @@ def receive_image():
         
     return 'upload'
 
+# 填姓名+邮箱并录入
 @app.route('/sendInfo',methods=['POST'])
 def sendInfo():
     data=request.form.to_dict()
@@ -47,10 +54,12 @@ def sendInfo():
     print('name:{}'.format(name),'email:{}'.format(email))
     return redirect(url_for('hello_world'))
  
+# 删除信息
 @app.route('/manageInfo',methods=['GET','POST'])
 def manageInfo():
     return render_template('manageInfo.html')
 
+# 登录界面
 @app.route('/login',methods=['POST','GET'])
 def login():
     if globals.loginState==0:
@@ -61,15 +70,25 @@ def login():
     else:
         return redirect(url_for('hello_world'))
 
-@app.route('/loginCheck',methods=['POST','GET'])
+# 检查是否为管理员
+@app.route('/loginCheck',methods=['GET','POST'])
 def loginCheck():
-    password=request.form.get('pw')
+    password=request.args.get('password')
     print(password)
     if globals.password==password:
+        globals.loginState=1
         return redirect(url_for('hello_world'))
     else:
         globals.retry=1
         return redirect(url_for('login'))
+
+# 管理员页面
+@app.route('/adminPage',methods=['GET','POST'])
+def adminPage():
+    return render_template(
+        'adminPage.html'
+        )
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=5000)
