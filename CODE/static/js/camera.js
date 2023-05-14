388ba043f51c22dd2006e333bfeb9cb11887d73e
王旭刚 <castamerego@gmail.com>
2023-05-14 20:58:27 +0800

    尝试使用socket监测并重复尝试连接来解决断连问题

--- CODE/static/js/camera.js
+++ CODE/static/js/camera.js
@@ -13,46 +13,42 @@ while (true) {
   }
 }
 
-let ip=Server.ip
+  let ip=Server.ip
 
-// 定义函数用于连接 socket
-function connect() {
-  let socket = new WebSocket(ip);
-  // 监听连接成功事件
-  socket.onopen = function() {
-    console.log('Socket已连接');
-  };
-  // 监听连接失败事件
-  socket.onerror = function() {
-    console.log('连接失败，3秒后重试');
-    setTimeout(function() {
-      connect(); // 延迟3秒后再次尝试连接
-    }, 3000);
-  };
-}
-
-// 调用连接函数
-connect();
-
-//shake hands with server :send hello
-socket.onopen = function (event) {
-    socket.send("hello");
-    //receive message from server,if message is "123",start sending video frames
-    socket.onmessage = function (event) {
-        if (event.data == "123") {
-            console.log("123");
-            sendFrame();
-        }
+  function connect() {
+    socket = new WebSocket(ip);
+    socket.onopen = function(event) {
+      socket.send("hello");
+      console.log('WebSocket connected');
+    };
+    socket.onmessage = function(event) {
+      if (event.data == "123") {
+        console.log("123");
+        sendFrame();
     }
-}
-// Get the video frames to send to the server
-function sendFrame() {var canvas = document.createElement('canvas');
-  var context = canvas.getContext('2d');
-  canvas.width = videoPreview.videoWidth;
-  canvas.height = videoPreview.videoHeight;
-  context.drawImage(videoPreview, 0, 0, canvas.width, canvas.height);
-  var dataURL = canvas.toDataURL('image/jpeg', 0.5);
-  socket.send(dataURL);
-  setTimeout(sendFrame, 100);
-}
+    };
+    socket.onerror = function(event) {
+      console.error('WebSocket error: ', event);
+    };
+    socket.onclose = function(event) {
+      console.warn('WebSocket closed, reconnecting in 1000ms...');
+      setTimeout(connect, 1000);
+    };
+  }
+
+  connect();
+
+  // Get the video frames to send to the server
+  function sendFrame() {
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+    canvas.width = videoPreview.videoWidth;
+    canvas.height = videoPreview.videoHeight;
+    context.drawImage(videoPreview, 0, 0, canvas.width, canvas.height);
+    var dataURL = canvas.toDataURL('image/jpeg', 0.5);
+    if (socket.readyState === WebSocket.OPEN) { // 只有socket状态为OPEN时才发送数据
+      socket.send(dataURL);
+    }
+    setTimeout(sendFrame, 100);
+  }
 
